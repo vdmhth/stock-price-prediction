53b550256abc8facdfe1953bb9fc968da0a0d338
maythepd <109785448+maythepd@users.noreply.github.com>
2026-06-02 14:52:21 +0700

    Delete src directory

--- src/data/crawl/config.py
+++
@@ -1,9 +0,0 @@
-STOCK_CODES = ["VCB", "FPT", "VIC", "HPG", "VNM"]
-MARKET_INDICES = ["VNINDEX", "VN30"]
-START_DATE = '2000-01-01'
-END_DATE = '2025-12-31'
-RAW_STOCK_DIR = 'data/raw/stock/'
-RAW_MARKET_DIR = 'data/raw/market/'
-METADATA_DIR = 'data/raw/metadata/'
-PROCESSED_STOCK_DIR = 'data/processed/stock/'
-INTERVAL = '1D'
